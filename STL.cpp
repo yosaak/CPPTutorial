e3ddf6514cafc180dc97c07594db527cabdf0f8e
anusha <74725802+yosaak@users.noreply.github.com>
2023-02-15 01:29:25 +0000

    Create STL.cpp

---
+++ STL.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/* stl queue, stack, list, tree, template classes, adaptor classes */
+/* use constructor, destructor, inheritance, smart pointer*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
