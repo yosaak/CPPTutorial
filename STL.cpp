8db74145b62a6d22b28ec60a31db055de49b9b96
anusha <74725802+yosaak@users.noreply.github.com>
2023-02-16 22:58:13 +0000

    Init commit

--- STL.cpp
+++ STL.cpp
@@ -1,21 +1,4 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-/* stl queue, stack, list, tree, template classes, adaptor classes */
-/* use constructor, destructor, inheritance, smart pointer*/
-
 #include <iostream>
+/*includes all headers*/
+#include<bits/stdc++.h>
 
-using namespace std;
-
-int main()
-{
-    cout<<"Hello World";
-
-    return 0;
-}
